81fab57430ff1c5292a2b5925ab95956a3e334a6
Dima Simagin <dimasimagin@outlook.com>
2022-02-26 00:38:32 +0100

    updates in customizetabwidget

--- UI/customizetabwidget.py
+++ UI/customizetabwidget.py
@@ -1,26 +1,32 @@
-from PyQt5.QtWidgets import QVBoxLayout,QLabel,QWidget, QHBoxLayout, QCalendarWidget
-from PyQt5.QtCore import Qt, QDate
+from PyQt5.QtWidgets import QVBoxLayout,QLabel,QWidget, QHBoxLayout, QCalendarWidget, QToolButton
+from PyQt5.QtCore import Qt, QDate, QSize
 from PyQt5.QtGui import QFont
 
 
 
 class CustomizeTabWidget(QWidget):
-	def __init__(self):
+	def __init__(self, vehicle):
 		super().__init__()
 		vbox = QVBoxLayout()
 		self.calendar = QCalendarWidget()
-		self.calendar.setGridVisible(True)
+		self.calendar.setGridVisible(False)
 		self.calendar.selectionChanged.connect(self.calendar_date)
 		self.calendar.showToday()
-		self.calendar.setMinimumDate(QDate.addDays(QDate.currentDate(),-5))
-		self.calendar.setMaximumDate(QDate.addDays(QDate.currentDate(),+5))
+		self.calendar.setMinimumDate(QDate.addDays(QDate.currentDate(),-7))
+		self.calendar.setMaximumDate(QDate.addDays(QDate.currentDate(),+7))
+		self.calendar.activated.connect(lambda: print("hello"))		# double click event
+		self.calendar.setNavigationBarVisible(False)
 
-		#self.label =QLabel("Hello")
-		#self.label.setFont(QFont("Sanserif", 15))
-		#self.label.setStyleSheet('color:red')
+		self.vehicle = vehicle
+
+		fn = self.calendar.font()
+		fn.setPointSize(10)
+		self.calendar.setFont(fn)
+
+		self.label = QLabel()
 
 		vbox.addWidget(self.calendar)
-		#vbox.addWidget(self.label)
+		vbox.addWidget(self.label)
 
 		self.setLayout(vbox)
 
@@ -28,5 +34,13 @@ class CustomizeTabWidget(QWidget):
 		dateselected = self.calendar.selectedDate()
 		date_in_string = str(dateselected.toPyDate())
 
-		#self.label.setText("Date Is : " + date_in_string)
+		self.label.setText("Date Is : " + date_in_string)
+
+
+	def setNavigationBarSize(self,width,height):
+		prev_button = self.calendar.findChild(QToolButton, "qt_calendar_prevmonth")
+		next_button = self.calendar.findChild(QToolButton, "qt_calendar_nextmonth")
+		for btn in (prev_button, next_button):
+			btn.setIconSize(QSize(width, height))
+
 
