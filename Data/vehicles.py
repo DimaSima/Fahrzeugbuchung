e6e7c8048de3fca0a80e3da528e8b4325d6ed1a7
Dima Simagin <dimasimagin@outlook.com>
2022-03-02 22:10:20 +0100

    added new member variables + added pandas dataframe for loading excel file

--- Data/vehicles.py
+++ Data/vehicles.py
@@ -1,5 +1,6 @@
 
 from enum import Enum
+import pandas as pd
 
 from Data.vehicle import Vehicle
 
@@ -13,16 +14,29 @@ class Models(Enum):
 class Vehicles:
 	def __init__(self):
 		self.allVehicles = []
-		self.allVehiclesName = ["J1", "992", "E3", "Macan"]
-		self.countAllVehicles = len(self.allVehiclesName)
+
+		self.excelData = pd.read_excel('Fahrzeuge.xlsx')
+		self.allVehiclesModel = self.excelData.loc[:,'Model']
+		self.countAllVehicles = len(self.allVehiclesModel)
+		self.allVehiclesVIN = self.excelData.loc[:,'VIN']
+		self.allVehiclesKennzeichen = self.excelData.loc[:,'Kennzeichen']
+		self.allVehiclesMIBSW = self.excelData.loc[:,'MIB SW']
 
 		self.loadAllVehicles()
 
 
 
 	def loadAllVehicles(self):
-		for v in self.allVehiclesName:
-			vehicleModel = Vehicle(v)
+		#for v in self.allVehiclesName:
+		#	vehicleModel = Vehicle(v)
+		#	self.allVehicles.append(vehicleModel)
+
+		for i in range(self.countAllVehicles):
+			model = self.allVehiclesModel[i]
+			vin = self.allVehiclesVIN[i]
+			kennzeichen = self.allVehiclesKennzeichen[i]
+			mib_sw = self.allVehiclesMIBSW[i]
+			vehicleModel = Vehicle(model,vin,kennzeichen,mib_sw)
 			self.allVehicles.append(vehicleModel)
 
 
