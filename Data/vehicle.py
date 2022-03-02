f22ef24af029e24b0239b7a087a2da5c527e9fce
Dima Simagin <dimasimagin@outlook.com>
2022-03-02 22:09:41 +0100

    added new member variables

--- Data/vehicle.py
+++ Data/vehicle.py
@@ -1,16 +1,20 @@
 
 
 class Vehicle:
-	def __init__(self, name): 
-		self.name = name
-		self.vin = 0
-		self.mib_sw = None
+	def __init__(self, model, vin, kennzeichen, mib_sw): 
+		self.model = model
+		self.vin = vin
+		self.kennzeichen = kennzeichen
+		self.mib_sw = mib_sw
 		self.mib_hw = None
 
 
-	def getName(self):
-		return self.name
+	def getModel(self):
+		return self.model
 
 	def getVin(self):
 		return self.vin
+
+	def getKennzeichen(self):
+		return self.kennzeichen
 		
